37e9fdfd345fdf1b1b96b1390a80caaa5f573f3e
sregula91 <sregula91@users.noreply.github.com>
2020-09-19 11:43:32 +1200

    Webstite using React MDL
    To Run the Site and install React MDL
    npm install --save react-mdl
    
    also install
    React DOM router :"
    npm install react react-dom"

---
+++ src/components/analyst.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+  
+class Analyst extends Component
+{
+    render()
+    {
+        return (<div>
+            <h1> Analyst </h1>
+        </div>)
+    }
+}
+export default Analyst;
---
+++ src/components/moderator.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+  
+class Moderator extends Component
+{
+    render()
+    {
+        return (<div>
+            <h1> Moderator</h1>
+        </div>)
+    }
+}
+export default Moderator;

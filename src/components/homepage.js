5ba3e675aa658210d2fcddb8359ff05cfda2677f
sregula91 <sregula91@users.noreply.github.com>
2020-09-20 08:18:07 +1200

    Updated home page

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,20 +1,30 @@
 import React, {Component} from 'react';
-import { Grid, Cell,Card,CardTitle,CardActions, Button, CardMenu, IconButton } from 'react-mdl';
+import { Grid, Cell,Card,CardText,CardTitle,CardActions, Button, CardMenu, IconButton } from 'react-mdl';
   
-class Home extends Component
-{
+
+class Home extends Component {
     render()
     {
         return (
         <div style ={{width:'100%',margin: 'auto'}} >
+            <body>
            <Grid className="landing-grid">
                <Cell col={12}>
-              <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
-                  <CardTitle style ={{color:'#fff',height:'175px',background:'url() center/cover'}}> Your Search End's Here</CardTitle>
-              </Card>
+               <Card shadow={0} style={{width: '512px', margin: 'auto'}}>
+    <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Welcome</CardTitle>
+    <CardText>
+        Welcome to SEER Site.
+    </CardText>
+    <CardActions border>
+        <Button colored>Get Started</Button>
+    </CardActions>
+    <CardMenu style={{color: '#fff'}}>
+        <IconButton name="share" />
+    </CardMenu>
+</Card>
                </Cell>
            </Grid>
-
+           </body>
         </div>
         )
     }

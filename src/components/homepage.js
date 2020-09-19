2cdb21e5bc119bc5720f3c3504e0ce120ff78c29
sregula91 <sregula91@users.noreply.github.com>
2020-09-19 16:53:02 +1200

    Added a card for home page to Add the Search bar

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,12 +1,28 @@
 import React, {Component} from 'react';
+import { Grid, Cell,Card,CardTitle,CardActions, Button, CardMenu, IconButton } from 'react-mdl';
   
 class Home extends Component
 {
     render()
     {
-        return (<div>
-            <h1> Home Page</h1>
-        </div>)
+        return (
+        <div style ={{width:'100%',margin: 'auto'}} >
+           <Grid className="landing-grid">
+               <Cell col={12}>
+              <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
+                  <CardTitle style ={{color:'#fff',height:'175px',background:'url() center/cover'}}> Your Search End's Here</CardTitle>
+              </Card>
+
+
+
+
+
+
+               </Cell>
+           </Grid>
+
+        </div>
+        )
     }
 }
 export default Home;

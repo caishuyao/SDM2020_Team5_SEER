4da130e55baeeb9ae3966f8448d5fe30e4dc0198
Jian Chen <key2chan@gmail.com>
2020-09-21 02:51:43 +1200

    test

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React from 'react';
 
 import './App.css';
 
-import {Link} from 'react-router-dom';
-import {Container,Row,Col} from 'react-bootstrap';
-import{Navbar,Nav,NavDropdown,Button,Jumbotron} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Button, Jumbotron } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
+import Analyst from './Components/analyst';
+import Test from './Components/test';
+
+console.log(Test);
 
 function App() {
-  return (
-<Header/>
-   
-  );
-}
+    return (
+      <div>
+        {/* <Header/>
+        <Analyst/> */}
+        <Test/>
+      </div>
+    );
+};
 
 export default App;
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,59 +1,58 @@
-import React, {Component} from 'react';
-import {Container,Row,Col} from 'react-bootstrap';
-import{Navbar,Nav,NavDropdown,Button,Jumbotron,FormControl,FormGroup,Form} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Button, Jumbotron, FormControl, FormGroup, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Image from 'react-bootstrap/Image';
 
 class Home extends Component {
-    render()
-    {
-        return (
-        <div>
-            <Container>
+  render() {
+    return (
+      <div>
+        <Container>
 
-  <Row><Col><Jumbotron>
-  <p>
-  <div>
-  <InputGroup className="mb-3">
-    <FormControl
-      placeholder="Search"
-      aria-label="Recipient's username"
-      aria-describedby="basic-addon2"
-    />
-   
-    <InputGroup.Append>
-      <Button as="input" type="submit" value="Submit" ></Button>
-    </InputGroup.Append>
-  </InputGroup>
-  <Form>
-  <Form.Group controlId="formBasicRange">
-    <Form.Label> Date Range</Form.Label>
-    <Form.Control type="range" />
-  </Form.Group>
-</Form>
-  </div>
-  </p>
-  <p>
+          <Row><Col><Jumbotron>
+            <p>
+              <div>
+                <InputGroup className="mb-3">
+                  <FormControl
+                    placeholder="Search"
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                  />
 
-  </p>
-</Jumbotron></Col></Row>
+                  <InputGroup.Append>
+                    <Button as="input" type="submit" value="Submit" ></Button>
+                  </InputGroup.Append>
+                </InputGroup>
+                <Form>
+                  <Form.Group controlId="formBasicRange">
+                    <Form.Label> Date Range</Form.Label>
+                    <Form.Control type="range" />
+                  </Form.Group>
+                </Form>
+              </div>
+            </p>
+            <p>
 
-<Row><Col><Jumbotron>
-  <h3>Welcome to our SEER</h3>
-  <p>
- Display number of articles, year of articles, and author of articles.
-  </p>
-  <p>
-   //cards here
-  </p>
-</Jumbotron></Col></Row>
+            </p>
+          </Jumbotron></Col></Row>
 
-</Container>
-  
-</div>
-       
-        )
-    }
+          <Row><Col><Jumbotron>
+            <h3>Welcome to our SEER</h3>
+            <p>
+              Display number of articles, year of articles, and author of articles.
+  </p>
+            <p>
+              //cards here
+  </p>
+          </Jumbotron></Col></Row>
+
+        </Container>
+
+      </div>
+
+    )
+  }
 }
 export default Home;

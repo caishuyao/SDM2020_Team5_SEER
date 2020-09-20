c4e9f25b7eb28439525476daebd8aaeebba2db65
Jian Chen <key2chan@gmail.com>
2020-09-21 03:15:35 +1200

    copy master

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/software development method/i);
+  expect(linkElement).toBeInTheDocument();
+});

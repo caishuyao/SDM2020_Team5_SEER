203b381ee427c4d951d0602d2ce8a861bc6bd286
Jian Chen <key2chan@gmail.com>
2020-09-21 11:21:04 +1200

    save

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 import { Navbar, Nav, NavDropdown, Button, Jumbotron } from 'react-bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header';
-import Analyst from './Components/analyst';
+import Analyst from './Components/analyst.js';
 import Test from './Components/test';
 
 console.log(Test);

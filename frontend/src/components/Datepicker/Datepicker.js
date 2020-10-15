3cb386d06903ce86407dc8a10d355c2d99d0ab82
Jian Chen <key2chan@gmail.com>
2020-10-15 20:37:30 +1300

    Remove no used post state

---
+++ frontend/src/components/Datepicker/Datepicker.js
@@ -0,0 +1,84 @@
+import React from "react";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import { FormControl, InputLabel, Input } from "@material-ui/core";
+//import { FormControl, InputLabel, TextField } from "@material-ui/core";
+// @material-ui/icons
+import Clear from "@material-ui/icons/Clear";
+import Check from "@material-ui/icons/Check";
+// core components
+import styles from "assets/jss/material-dashboard-react/components/customInputStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function DatePicker(props) {
+  const classes = useStyles();
+  const {
+    formControlProps,
+    labelText,
+    id,
+    labelProps,
+    inputProps,
+    error,
+    success,
+  } = props;
+
+  const labelClasses = classNames({
+    [" " + classes.labelRootError]: error,
+    [" " + classes.labelRootSuccess]: success && !error,
+  });
+  const underlineClasses = classNames({
+    [classes.underlineError]: error,
+    [classes.underlineSuccess]: success && !error,
+    [classes.underline]: true,
+  });
+  const marginTop = classNames({
+    [classes.marginTop]: labelText === undefined,
+  });
+
+  return (
+    <FormControl
+      {...formControlProps}
+      className={formControlProps.className + " " + classes.formControl}
+    >
+      {labelText !== undefined ? (
+        <InputLabel
+          className={classes.labelRoot + labelClasses}
+          htmlFor={id}
+          {...labelProps}
+        >
+          {labelText}
+        </InputLabel>
+      ) : null}
+
+      <Input
+        type="date"
+        classes={{
+          root: marginTop,
+          disabled: classes.disabled,
+          underline: underlineClasses,
+        }}
+        defaultValue={new Date().toLocaleDateString("zh-CN")}
+        id={id}
+        {...inputProps}
+      />
+      {error ? (
+        <Clear className={classes.feedback + " " + classes.labelRootError} />
+      ) : success ? (
+        <Check className={classes.feedback + " " + classes.labelRootSuccess} />
+      ) : null}
+    </FormControl>
+  );
+}
+
+DatePicker.propTypes = {
+  labelText: PropTypes.node,
+  labelProps: PropTypes.object,
+  id: PropTypes.string,
+  inputProps: PropTypes.object,
+  formControlProps: PropTypes.object,
+  error: PropTypes.bool,
+  success: PropTypes.bool,
+};

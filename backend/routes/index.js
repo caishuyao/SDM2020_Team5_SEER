0ae673012938b796eb06d476149485b8b91d2077
charles cai <caishuyao@gmail.com>
2020-10-16 15:27:51 +1300

    fix the static path

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,7 +8,7 @@ router.use('/api/article', articleRoute);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-	res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+	res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
 });
 
 //router.use(function(req, res) {
